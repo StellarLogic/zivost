adab4e5a6421a4a01b82b6c743b2e37ebb818cf3
akdenied <akdenied@gmail.com>
2020-08-13 01:49:50 +0530

    Updates

---
+++ precache-manifest.360cafa26dc8c52874874f52ba62aadc.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77ba56c94ee945dbecf1dd49769e2697",
+    "url": "/zivost/index.html"
+  },
+  {
+    "revision": "aaeaa58998bc5f5fd8e3",
+    "url": "/zivost/static/css/2.0e7909e3.chunk.css"
+  },
+  {
+    "revision": "e71406c2155e358b6268",
+    "url": "/zivost/static/css/main.81ac87e2.chunk.css"
+  },
+  {
+    "revision": "aaeaa58998bc5f5fd8e3",
+    "url": "/zivost/static/js/2.42b17c8d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/zivost/static/js/2.42b17c8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e71406c2155e358b6268",
+    "url": "/zivost/static/js/main.1528de74.chunk.js"
+  },
+  {
+    "revision": "a85f0129ec15ea58c443",
+    "url": "/zivost/static/js/runtime-main.5a0b7752.js"
+  },
+  {
+    "revision": "8acb4de04d3ae29325ebd58174f127ae",
+    "url": "/zivost/static/media/AvenirLTStd-Book.8acb4de0.woff2"
+  },
+  {
+    "revision": "2bd52a72cd1b29902b682bb732f7f52a",
+    "url": "/zivost/static/media/AvenirLTStd-Medium.2bd52a72.woff2"
+  },
+  {
+    "revision": "f64a5b3a00def0f372b7cfa031afa722",
+    "url": "/zivost/static/media/background-copy.f64a5b3a.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/zivost/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/zivost/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/zivost/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/zivost/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/zivost/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "9b67543a4cfaaacba85265611353b916",
+    "url": "/zivost/static/media/logo-complete@2x.9b67543a.png"
+  },
+  {
+    "revision": "70f1f76f9b20c9307a3c459b1a281e28",
+    "url": "/zivost/static/media/profile-1.70f1f76f.jpg"
+  },
+  {
+    "revision": "c66ed6c4b7b1898571d7d14972ec31b1",
+    "url": "/zivost/static/media/profile-2.c66ed6c4.jpg"
+  },
+  {
+    "revision": "97491a8f857430a38a96853c0f2931e5",
+    "url": "/zivost/static/media/profile-3.97491a8f.jpg"
+  },
+  {
+    "revision": "36d6b0677421b8609d80bee67d027896",
+    "url": "/zivost/static/media/profile-4.36d6b067.jpg"
+  }
+]);
